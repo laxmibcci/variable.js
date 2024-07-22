b0f503bf191851f48268c5250f8f47c36554db72
Deepak Kumar <deepak@Deepaks-MacBook-Air.local>
2024-07-22 14:57:52 +0530

    Add initial files

---
+++ index.js
@@ -0,0 +1,49 @@
+// var age = 25;
+
+
+// if(true) {
+//     console.log(age);
+// }
+
+// function solve() {
+//     var age = 25;
+//     console.log(age);
+// }
+
+// console.log(age);
+// solve();
+
+// var x = 10;
+// var x = 20;
+
+// {
+// let a = 20;
+// console.log(a);
+// }
+
+// let a = 20;
+// a = 30;
+
+// let a = 30;
+// a = mayank;
+// a = laxmi;
+// a = saari;
+
+// const a = 28;
+// console.log(a);
+
+// let marks = 20.3566;
+// marks = "laxmi"
+// marks = "true"
+
+// let marks = 10389876655354657878786756453018747583474243565767878656455798498983988783776756376874378576467865784368794570827876476;
+// console.log(marks);
+
+// marks = null;
+// console.log(typeof(marks));
+
+let mysymbol = Symbol('description');
+console.log(mysymbol);
+
+
+
